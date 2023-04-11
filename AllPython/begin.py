7b06157bb7a0a88a927c6e898865043e60075aa7
toyosee <tyabolaji@gmail.com>
2023-04-11 13:02:36 +0100

    new code

--- AllPython/begin.py
+++ AllPython/begin.py
@@ -1,1 +1,12 @@
-print("hello world")
+try:
+    
+    even_numbers = [2,4,6,8]
+    print(even_numbers[0])
+
+except ZeroDivisionError:
+    print("Denominator cannot be 0.")
+    
+except IndexError:
+    print("Index Out of Bound.")
+
+# Output: Index Out of Bound
